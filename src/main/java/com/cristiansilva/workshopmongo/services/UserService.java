03043f033072f7c062163a776fc69c3ab1ad630d
Cristian Silva <cristiansilvazzzz1@gmail.com>
2022-11-25 14:18:38 -0300

    Inserção de usuário com POST

--- src/main/java/com/cristiansilva/workshopmongo/services/UserService.java
+++ src/main/java/com/cristiansilva/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cristiansilva.workshopmongo.domain.User;
+import com.cristiansilva.workshopmongo.dto.UserDTO;
 import com.cristiansilva.workshopmongo.repository.UserRepository;
 import com.cristiansilva.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -22,5 +23,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 } 
