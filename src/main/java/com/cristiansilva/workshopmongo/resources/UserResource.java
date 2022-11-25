ac75192985fe48b24df4d9bf9d3ddb5e3fa44eb8
Cristian Silva <cristiansilvazzzz1@gmail.com>
2022-11-25 16:57:03 -0300

    Atualização de usuário com PUT

--- src/main/java/com/cristiansilva/workshopmongo/resources/UserResource.java
+++ src/main/java/com/cristiansilva/workshopmongo/resources/UserResource.java
@@ -45,9 +45,17 @@ public class UserResource {
 		return ResponseEntity.created(uri).build();
 	} 
 	
-	@RequestMapping( method = RequestMethod.DELETE)
+	@RequestMapping(value="/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable String id) {
-		userService.Delete(id);
+		userService.delete(id);
+		return ResponseEntity.noContent().build();
+	} 
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = userService.fromDTO(objDto);
+		obj.setId(id);
+		obj = userService.update(obj);
 		return ResponseEntity.noContent().build();
 	} 
 }
--- src/main/java/com/cristiansilva/workshopmongo/services/UserService.java
+++ src/main/java/com/cristiansilva/workshopmongo/services/UserService.java
@@ -13,30 +13,41 @@ import com.cristiansilva.workshopmongo.services.exception.ObjectNotFoundExceptio
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return userRepository.findAll();
-	} 
+	}
+
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return userRepository.insert(obj);
 	}
-	
-	public void Delete(String id) {
+
+	public void delete(String id) {
 		findById(id);
 		userRepository.deleteById(id);
+	}
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return userRepository.save(newObj);
+		}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
 	}
 	
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
-	
 
-} 
+}
