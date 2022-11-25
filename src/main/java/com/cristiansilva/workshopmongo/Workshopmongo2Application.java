3618c7d9a40f3c1edbe59ad57b032f5c8279040f
Cristian Silva <cristiansilvazzzz1@gmail.com>
2022-11-25 12:48:31 -0300

    first commit

---
+++ src/main/java/com/cristiansilva/workshopmongo/Workshopmongo2Application.java
@@ -0,0 +1,13 @@
+package com.cristiansilva.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshopmongo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshopmongo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cristiansilva/workshopmongo/Workshopmongo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristiansilva.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshopmongo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
