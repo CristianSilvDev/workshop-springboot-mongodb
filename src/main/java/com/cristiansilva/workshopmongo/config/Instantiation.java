751148b9b29f455b4ac64b6c55ca4e7255205a47
Cristian Silva <cristiansilvazzzz1@gmail.com>
2022-11-25 17:59:36 -0300

    Referenciando os posts do usuário

--- src/main/java/com/cristiansilva/workshopmongo/config/Instantiation.java
+++ src/main/java/com/cristiansilva/workshopmongo/config/Instantiation.java
@@ -42,9 +42,11 @@ public class Instantiation implements CommandLineRunner {
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 
 }
